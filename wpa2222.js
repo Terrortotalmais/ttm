065308a5fe906377f53a18ae23edae64f017080a
Terrortotalmais <97311397+Terrortotalmais@users.noreply.github.com>
2022-01-14 21:57:23 -0300

    Update wpa2222.js

--- wpa2222.js
+++ wpa2222.js
@@ -1,88 +1,20 @@
-
-	if (getPWADisplayMode() === 'standalone') {
-		$('#installApp').hide();
-	}
-
-	window.matchMedia('(display-mode: standalone)').addEventListener('change', ({ matches }) => {
-		if (matches) {
-			$('#installApp').hide();
-		} else {
-			$('#installApp').show();
-		}
-	});
-
-	document.onkeydown = function (event) {
-		event = event || window.event;
-
-		if (event.key === 'Escape') {
-			$('#aboutModal').modal('hide');
-		} else if (event.ctrlKey && event.code === 'KeyS') {
-			saveTextAsFile(note.value, getFileName());
-			event.preventDefault();
-		}
-	};
-});
-
-// Registering ServiceWorker
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js').then(function (registration) {
-		console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	}).catch(function (err) {
-		console.log('ServiceWorker registration failed: ', err);
-	});
-}
-
 let deferredPrompt;
-let installSource;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-	$('.install-app-btn-container').show();
-	deferredPrompt = e;
-	installSource = 'nativeInstallCard';
-
-	e.userChoice.then(function (choiceResult) {
-		if (choiceResult.outcome === 'accepted') {
-			deferredPrompt = null;
-		}
-
-		ga('send', {
-			hitType: 'event',
-			eventCategory: 'pwa-install',
-			eventAction: 'native-installation-card-prompted',
-			eventLabel: installSource,
-			eventValue: choiceResult.outcome === 'accepted' ? 1 : 0
-		});
-	});
+    deferredPrompt = e;
 });
-
 const installApp = document.getElementById('installApp');
 
 installApp.addEventListener('click', async () => {
-	installSource = 'customInstallationButton';
-
-	if (deferredPrompt !== null) {
-		deferredPrompt.prompt();
-		const { outcome } = await deferredPrompt.userChoice;
-		if (outcome === 'accepted') {
-			deferredPrompt = null;
-		}
-
-		ga('send', {
-			hitType: 'event',
-			eventCategory: 'pwa-install',
-			eventAction: 'custom-installation-button-clicked',
-			eventLabel: installSource,
-			eventValue: outcome === 'accepted' ? 1 : 0
-		});
-	} else {
-		showToast('Notepad is already installed.')
-	}
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+        }
+    }
 });
-
-window.addEventListener('appinstalled', () => {
-	deferredPrompt = null;
-
-	const source = installSource || 'browser';
-
-	ga('send', 'event', 'pwa-install', 'installed', source);
+window.addEventListener('beforeinstallprompt', (e) => {
+    $('.install-app-btn-container').show();
+    deferredPrompt = e;
 });
